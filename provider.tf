4fa97d3777130ddb2fac66cb0a8afa3214910ba0
sofiaponteb <aaponte@unal.edu.co>
2023-02-21 11:58:27 -0500

    error fix APIkey

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ variable "IC_API_Key" {}
 variable "region" {}
 
 provider "ibm" {
-    ibmcloud_api_key   = var.ibmcloud_api_key
+    ibmcloud_api_key   = var.IC_API_Key
     region = var.region
     }
